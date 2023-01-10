773200a897af928acf2c253d6998df765a804c8f
DelosReyes-Rolly <rollydelosreyes729@gmail.com>
2023-01-10 22:58:10 +0800

    breadcrumbs level 2

--- resources/views/student/documentrequestPreview.blade.php
+++ resources/views/student/documentrequestPreview.blade.php
@@ -4,8 +4,9 @@
 
 
 	<section>
+	<div style="font-size: 20px;"><a href="/studentrequest"> Document Request</a>&emsp;<i class="fas fa-angle-right"></i>&emsp; view file</div><br/>
 		<div style="margin: 0px;">
-            <a class="btn btn-secondary btn-lg" href="/studentrequest" style="float: right; font-size: 18px;"><i class="fas fa-arrow-left"></i>   Back to document request</a>
+            <a class="btn btn-secondary btn-lg" href="/studentrequest" style="float: right; font-size: 18px;"><i class="fas fa-arrow-left"></i>  Back to document request</a><br/><br/><br/><br/> 
         </div>
 		<center><img src="/views/{{$requests -> file}}" style="width:max-content; height:max-content; display: block;"></center>
 	</section>
