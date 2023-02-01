507c13183559eaeaf4ffa2f0b6c46018c1947dfe
DelosReyes-Rolly <rollydelosreyes729@gmail.com>
2023-02-01 15:14:17 +0800

    asd

--- resources/views/admins/landing/privateannouncement.blade.php
+++ resources/views/admins/landing/privateannouncement.blade.php
@@ -1,9 +1,10 @@
 @include('partials.adminheader')
 @include('partials.adminSecondHeader')
 <main>
+<script src="{{ asset('assets/js/needs-validated.js') }}"></script>
         <!-- form -->
     <div class="left-to-right">
-        <form method="POST" action="/add/privateannouncements" enctype="multipart/form-data"class="needs-validation" novalidate>
+        <form method="POST" id="createPrivateannouncement" class="needs-validation" novalidate>
             @csrf
             <div class="container-xl px-4 mt-4 left-to-right">
                 <!-- page navigation-->
@@ -15,23 +16,11 @@
                         <div class="card mb-4">
                             <div class="card border-start-lg border-start-yellow">
                                 <div class="card-header">
-                                    @if ($message = Session::get('message'))
-                                        <div class="alert alert-success alert-block">
-                                            <button type="button" class="close" data-dismiss="alert">×</button>
-                                            <strong>{{ $message }}</strong>
-                                        </div>
-                                    @endif
-                                                
-                                    @if (count($errors) > 0)
-                                        <div class="alert alert-danger">
-                                            <strong>Whoops!</strong> There were some problems with your input.
-                                            <ul>
-                                                @foreach ($errors->all() as $error)
-                                                    <li>{{ $error }}</li>
-                                                @endforeach
-                                            </ul>
-                                        </div>
-                                    @endif
+                                <div id="whoops" class="alert alert-danger" style="display: none;">
+                                    <b>Whoops! There is a problem in your input</b> <br/>
+                                    <div id="validation-errors"></div>
+                                </div>
+                                <center><div id="loadingDiv" style="color: red; font-weight: bold;"><div class="lds-hourglass"></div><br/> <div style="font-size: 20px;">Processing. Please wait...</div></div></center>
                                 </div>
                                 <div class="card-body" style="padding: 10px 40px 10px 40px">
                                     <div class="mb-3" style="color: red">
@@ -41,16 +30,16 @@
                                     <div class="row gx-3 mb-3">
                                         <!-- Form Group (title)-->
                                         <div class="col-md-6">
-                                            <label class="large mb-1" for="inputsubject" style="font-size: 20px;"><span style="color: red">*</span> Subject</label>
-                                            <input class="form-control @error('subject') is-invalid @enderror" id="inputsubject" type="text" placeholder="Enter the title" name="subject"  value="{{ old('subject') }}" required>
+                                            <label class="large mb-1" for="subject" style="font-size: 20px;"><span style="color: red">*</span> Subject</label>
+                                            <input class="form-control @error('subject') is-invalid @enderror" id="subject" type="text" placeholder="Enter the title" name="subject"  value="{{ old('subject') }}" required>
                                             <div class="invalid-feedback">
                                                 Please input subject.
                                             </div>
                                         </div>
                                         <!-- Form Group date-->
                                         <div class="col-md-3">
-                                            <label class="slarge mb-1" for="inputdate" style="font-size: 20px;"><span style="color: red">*</span> Date</label>
-                                            <input type="date" class="form-control @error('date') is-invalid @enderror" id="inputdate" placeholder="Enter the date" name="date"  value="{{ old('date') }}" required>
+                                            <label class="slarge mb-1" for="date" style="font-size: 20px;"><span style="color: red">*</span> Date</label>
+                                            <input type="date" class="form-control @error('date') is-invalid @enderror" id="date" placeholder="Enter the date" name="date"  value="{{ old('date') }}" required>
                                             <div class="invalid-feedback">
                                                 Please input date.
                                             </div>
@@ -70,15 +59,15 @@
                                     <div class="row gx-3 mb-3">
                                             <!-- Form Group whr-->
                                             <div class="col-md-3">
-                                                <label class="large mb-1" for="inputsender" style="font-size: 20px;"><span style="color: red">*</span> From</label>
-                                                <input class="form-control @error('sender') is-invalid @enderror" id="inputsender" type="text" placeholder="Enter the sender" name="sender"  value="{{ old('sender') }}" required>
+                                                <label class="large mb-1" for="sender" style="font-size: 20px;"><span style="color: red">*</span> From</label>
+                                                <input class="form-control @error('sender') is-invalid @enderror" id="sender" type="text" placeholder="Enter the sender" name="sender"  value="{{ old('sender') }}" required>
                                                 <div class="invalid-feedback">
                                                     Please input sender.
                                                 </div>
                                             </div>
                                             <div class="col-md-3">
-                                                <label class="large mb-1" for="inputrecipient" style="font-size: 20px;"><span style="color: red">*</span> To</label>
-                                                <input class="form-control @error('recipient') is-invalid @enderror" id="inputrecipient" type="text" placeholder="Enter the recipients" name="recipient"  value="{{ old('recipient') }}" required>
+                                                <label class="large mb-1" for="recipient" style="font-size: 20px;"><span style="color: red">*</span> To</label>
+                                                <input class="form-control @error('recipient') is-invalid @enderror" id="recipient" type="text" placeholder="Enter the recipients" name="recipient"  value="{{ old('recipient') }}" required>
                                                 <div class="invalid-feedback">
                                                     Please input recipient.
                                                 </div>
@@ -86,32 +75,33 @@
                                             <!-- Form Group whr-->
                                             <!-- Form Group (location)-->
                                             <div class="col-md-6">
-                                                <label class="large mb-1" for="inputlocation" style="font-size: 20px;"><span style="color: red">*</span> Location</label>
-                                                <input class="form-control @error('location') is-invalid @enderror" id="inputlocation" type="text" placeholder="Enter the location" name="location"  value="{{ old('location') }}" required>
+                                                <label class="large mb-1" for="location" style="font-size: 20px;"><span style="color: red">*</span> Location</label>
+                                                <input class="form-control @error('location') is-invalid @enderror" id="location" type="text" placeholder="Enter the location" name="location"  value="{{ old('location') }}" required>
                                                 <div class="invalid-feedback">
                                                     Please input location.
                                                 </div>
                                             </div>
-                                            <div class="col-md-2">
-                                                <br><label class="slarge mb-1" for="inputpost_expiration" style="font-size: 20px;"><span style="color: red">*</span> Post Expiration</label>
-                                                <input type="date" class="form-control @error('post_expiration') is-invalid @enderror" id="inputpost_expiration" placeholder="Enter the date" name="post_expiration"  value="{{ old('post_expiration') }}" required>
+                                            <div class="col-md-4">
+                                                <br><label for="post_expiration" style="font-size: 20px;"><span style="color: red">*</span> Post Expiration</label>
+                                                <input type="date" class="form-control @error('post_expiration') is-invalid @enderror" id="post_expiration" placeholder="Enter the date" name="post_expiration"  value="{{ old('post_expiration') }}" required>
                                                 <div class="invalid-feedback">
                                                     Please input expiry date.
                                                 </div>
                                             </div>
+                                            <!-- Form Group (content)-->
+                                            <div class="col-md-12">
+                                                <br/><label for="editor" style="font-size: 20px;"><span style="color: red">*</span> Content</label>
+                                                <textarea class="form-control @error('contents') is-invalid @enderror" id="contents" type="text" rows="10" cols="60" placeholder="Enter the information" name="contents" required>{{ old('content') }}</textarea>
+                                                <div class="invalid-feedback">
+                                                    Please input content.
+                                                </div>
+                                            </div><br/>
                                         </div><br/>
-                                        <!-- Form Group (content)-->
-                                        <div class="mb-3">
-                                            <label class="large mb-1" for="editor" style="font-size: 20px;"><span style="color: red">*</span> Content</label>
-                                            <textarea class="form-control @error('content') is-invalid @enderror" id="editor" type="text" placeholder="Enter the information" name="content"  rows="10" cols="80" required>{{ old('content') }}</textarea>
-                                            <div class="invalid-feedback">
-                                                Please input content.
-                                            </div>
-                                        </div><br/>
+                                        
                                         <div class="row gx-3 mb-3">
                                             <!-- Form Group (img)-->
                                             <div class="col-md-6">
-                                                <label class="large mb-1" for="inputcontent" style="font-size: 20px;">Image (Only png and jpg files are allowed)</label>
+                                                <label class="large mb-1" for="image" style="font-size: 20px;">Image (Only png and jpg files are allowed)</label>
                                                 <div class ="form-group row">
                                                     <div class="col-md-8"></div>
                                                     <input type="file" name = "image" class="form-control">
@@ -128,16 +118,10 @@
                 </div>
             </div>
         </form>
-        <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+        <!-- <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
         <script>
-            CKEDITOR.replace( 'editor' );
-        </script>
-        @if ($message = Session::get('approval'))
-            <div class="alert alert-success alert-block">
-                <button type="button" class="close" data-dismiss="alert">×</button>
-                <strong>{{ $message }}</strong>
-            </div>
-        @endif
+            CKEDITOR.replace( 'content' );
+        </script> -->
     <script type="text/javascript">
         $(document).ready(function(){
           $('.nav_btn').click(function(){
@@ -146,5 +130,48 @@
         });
     </script>
     <script src="{{ asset('assets/js/needs-validated.js') }}"></script>
+    <script>
+        var $loading = $('#loadingDiv').hide();
+        function formPost(){
+            $(document).ajaxStart(function () {
+                $loading.show();
+            })
+            .ajaxStop(function () {
+                $loading.hide();
+            });
+            $('#whoops').hide();
+            var form_data = $("form#createPrivateannouncement").serialize();
+            $(":submit").attr("disabled", true);
+            $.ajax({
+                type: "POST",
+                url: "{{ route('add.privateannouncmeent') }}",
+                data:form_data,
+                success: function(response) {
+                    if (response) {
+                        $("#createPrivateannouncement")[0].reset();
+                        $(":submit").removeAttr("disabled");
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success.',
+                            text: 'Private announcement has been added successfully',
+                        }).then(function() {
+                            location.reload(true);
+                        })
+                        
+                    }
+                },error: function (xhr) {
+                    $('#validation-errors').html('');
+                    document.getElementById('whoops').style.display = 'block';
+                    if(xhr.responseJSON.error != undefined){
+                        $('#validation-errors').append('&emsp;<li>'+xhr.responseJSON.error+'</li>');
+                    }
+                    $.each(xhr.responseJSON.errors, function(key,value) {
+                        $('#validation-errors').append('&emsp;<li>'+value+'</li>');
+                    }); 
+                    $(":submit").removeAttr("disabled");
+                },
+            });
+        }
+</script>
 </main>
 <br><br><br><br>
