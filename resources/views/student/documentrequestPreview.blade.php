4540c17bd363eb55f2b1554f629081ad252cc0c0
DelosReyes-Rolly <rollydelosreyes729@gmail.com>
2023-02-01 17:25:24 +0800

    asd

--- resources/views/student/documentrequestPreview.blade.php
+++ resources/views/student/documentrequestPreview.blade.php
@@ -4,6 +4,17 @@
 
 
 	<section>
+	<div style="font-size: 20px;">  
+		<nav  aria-label = "breadcrumb">
+            <!--Add the "breadcrumb" class to ul element that represents the breadcrumb-->
+            <ul class = "breadcrumb">
+               <!--Add the ".breadcrumb-item" class to each li element within the breadcrumb-->
+               <li class = "breadcrumb-item"><a class="bca" href = "/studentrequest"></a></li>
+               <!--Add the "active" class to li element to represent the current page-->
+               <li class = "breadcrumb-item active" aria-current = "page">Students</li>
+            </ul>
+         </nav>
+	</div>
 	<div style="font-size: 20px;"><a href="/studentrequest"> Document Request</a>&emsp;<i class="fas fa-angle-right"></i>&emsp; view file</div><br/>
 		<div style="margin: 0px;">
             <a class="btn btn-secondary btn-lg" href="/studentrequest" style="float: right; font-size: 18px;"><i class="fas fa-arrow-left"></i>  Back to document request</a><br/><br/><br/><br/> 
