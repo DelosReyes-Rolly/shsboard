61d1f1e3bf3a7a603f2d442ec34853572deefbe7
Rolly <rollydelosreyes729@gmail.com>
2023-03-21 13:55:32 +0800

    email

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,16 +1,15 @@
 @component('mail::message')
-# Account Created!
+
 
 <h3>Dear user,</h3><br>
 <p>
-    Your account has been created on SVNHS - SHS School Board!<br>
     This is a password for you to login your account.<br><br>
     <h1>{{$pass}}</h1><br><br>
     <br><br>
 </p>
 
     You can access your account in this link.
-@component('mail::button', ['url' => 'https://puptcapstone.net/shsboard/logins'])
+@component('mail::button', ['url' => 'https://shsboard.puptcapstone.net/logins'])
 Click here
 @endcomponent
 
