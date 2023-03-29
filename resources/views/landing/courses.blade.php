3a4d067e456860125d6d2e3b289138b5bb3142ef
Rolly <rollydelosreyes729@gmail.com>
2023-03-29 18:25:35 +0800

    x

--- resources/views/landing/courses.blade.php
+++ resources/views/landing/courses.blade.php
@@ -20,122 +20,17 @@
 						@else
 							@php $count = 0; @endphp
 							@foreach($strands as $strand)
-								@if($count !=6)
-									<div class="col-lg-4 col-md-12  left-to-right course-hover">
-										@if($strand->id == 1)
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a> @php $count++; @endphp
-										@elseif($strand->id == 2)
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a> @php $count++; @endphp
-										@elseif($strand->id == 3)
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a> @php $count++; @endphp
-										@elseif($strand->id == 4)
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a> @php $count++; @endphp
-										@elseif($strand->id == 5)
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a> @php $count++; @endphp
-										@elseif($strand->id == 6)
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a> @php $count++; @endphp
+							    <div class="col-lg-4 col-md-12  left-to-right course-hover">
+							        <a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
+										@if($strand->image == NULL)
+											<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
+										@else
+											<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
 										@endif
-									</div>
-								@endif
-							@endforeach
-							@foreach($strands as $strand)
-								<div class="course-hover">
-									@if($strand->id == 7)
-										<div class="col-sm-2 left-to-right course-hover"></div>
-										<div class="col-lg-4 left-to-right">
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a>
+										<div class="overlay-courses">
+											<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
 										</div>
-									@elseif($strand->id == 8)
-										<div class="col-lg-4 left-to-right course-hover">
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a>
-										</div>
-										<div class="col-sm-2 left-to-right course-hover"></div>
-									@elseif($strand->id >= 9)
-										<div class="col-lg-4 left-to-right course-hover">
-											<a class="card single_post course-box" href="{!! route('stranddescription',['id'=>$strand->id]) !!}">
-												@if($strand->image == NULL)
-													<img src="{{ URL::asset('img/shs.png')}}" class="course-image">
-												@else
-													<img src="{{ asset('img/'.$strand->image) }}" class="course-image"/>
-												@endif
-												<div class="overlay-courses">
-													<div class="text-courses">{{$strand->courseName}} ({{$strand->abbreviation}})</div>
-												</div>
-											</a>
-										</div>
-									@endif
+									</a> 
 								</div>
 							@endforeach
 						@endif
